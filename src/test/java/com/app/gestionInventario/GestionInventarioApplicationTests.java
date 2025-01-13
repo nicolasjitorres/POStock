bccec3e49e3896cbcb0a583f4aea6ed87aa24695
Nicolas Juan Ignacio Torres <nicoignacio123@gmail.com>
2025-01-12 22:42:08 -0300

    First commit

---
+++ src/test/java/com/app/gestionInventario/GestionInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gestionInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
