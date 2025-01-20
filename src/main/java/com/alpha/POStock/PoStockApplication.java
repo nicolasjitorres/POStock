902516d21b4b92f20ebc43533b34da3298afd7bb
Nicolas Juan Ignacio Torres <nicoignacio123@gmail.com>
2025-01-20 12:52:29 -0300

    FEAT: inicialización del proyecto POStock

---
+++ src/main/java/com/alpha/POStock/PoStockApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.POStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha/POStock/PoStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.POStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
